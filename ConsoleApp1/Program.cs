c8d1a673a64e651a381bf70472b80e1cd6f0f128
henrickmelovital <henrick.melo@yahoo.com.br>
2025-01-12 23:49:55 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using ConsoleApp1.Target;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        // Cálculo do valor da variável soma
+        Console.WriteLine("Soma: " + solution.Soma(indice: 13, soma: 0, K: 0));
+
+        // Verificação se o número pertence à sequência de Fibonacci
+        Console.WriteLine("\nInforme um número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (solution.PertenceFibonacci(numero))
+        {
+            Console.WriteLine($"O número {numero} pertence à sequência de Fibonacci.");
+        }
+        else
+        {
+            Console.WriteLine($"O número {numero} não pertence à sequência de Fibonacci.");
+        }
+
+        // Cálculo do faturamento diário
+        List<double> faturamentos = new List<double> { 22174.1664, 24537.6698, 26139.6134, 0.0, 0.0, 26742.6612, 0.0, 42889.2258 };
+        solution.FaturamentoDiarioValoresValidosMin(faturamentos);
+        solution.FaturamentoDiarioValoresValidosMax(faturamentos);
+        solution.FaturamentoDiarioValoresValidosMedia(faturamentos);
+        Console.WriteLine($"\nMenor valor: {solution.FaturamentoDiarioValoresValidosMin(faturamentos)}");
+        Console.WriteLine($"Maior valor: {solution.FaturamentoDiarioValoresValidosMax(faturamentos)}");
+        Console.WriteLine($"Dias acima da média: {solution.FaturamentoDiarioValoresValidosMedia(faturamentos)}");
+
+        // Cálculo do faturamento por estado
+        var faturamentoPorEstado = new Dictionary<string, double>
+        {
+            { "SP", 67836.43 },
+            { "RJ", 36678.66 },
+            { "MG", 29229.88 },
+            { "ES", 27165.48 },
+            { "Outros", 19849.53 }
+        };
+
+        var percentuaisPorEstado = solution.FaturamentoPorEstado(faturamentoPorEstado);
+        Console.WriteLine($"\nPercentual de faturamento por estado:");
+        foreach (var estado in percentuaisPorEstado)
+        {
+            Console.WriteLine($"{estado.Key}: {estado.Value:F2}%");
+        }
+
+        //Invert String
+        Console.WriteLine("\nInforme uma string: ");
+        string str = Console.ReadLine();
+        Console.WriteLine($"String invertida: {solution.InvertString(str)}");
+    }
+}
+
---
+++ ConsoleApp1/Target/Solution.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Target
+{
+    internal class Solution
+    {
+        public int Soma(int indice, int soma, int K)
+        {
+            while (K < indice)
+            {
+                K += 1;
+                soma += K;
+
+            }
+
+            return soma;
+        }
+
+        public bool PertenceFibonacci(int nuemro)
+        {
+            int a = 0;
+            int b = 1;
+            int temp;
+
+            while (a < nuemro)
+            {
+                temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a == 0;
+        }
+
+        public double FaturamentoDiarioValoresValidosMin(List<double> faturamentos)
+        {
+            var valoresValidos = faturamentos.Where(x => x > 0).ToList();
+            return valoresValidos.Min();
+        }
+
+        public double FaturamentoDiarioValoresValidosMax(List<double> faturamentos)
+        {
+            var valoresValidos = faturamentos.Where(x => x > 0).ToList();
+            return valoresValidos.Max();
+        }
+
+        public double FaturamentoDiarioValoresValidosMedia(List<double> faturamentos)
+        {
+            var valoresValidos = faturamentos.Where(x => x > 0).ToList();
+            double mediaMensal = valoresValidos.Average();
+            int diasAcimaDaMedia = valoresValidos.Count(v => v > mediaMensal);
+            return diasAcimaDaMedia;
+        }
+
+        public Dictionary<string, double> FaturamentoPorEstado(Dictionary<string, double> faturamentosPorEstado)
+        {
+            double faturamentoTotal = faturamentosPorEstado.Values.Sum();
+
+            Dictionary<string, double> percentuaisPorEstado = new Dictionary<string, double>();
+
+            foreach (var estado in faturamentosPorEstado)
+            {
+                double percentual = (estado.Value / faturamentoTotal) * 100;
+
+                percentuaisPorEstado.Add(estado.Key, percentual);
+            }
+
+            return percentuaisPorEstado;
+        }
+
+
+        public string InvertString(string texto)
+        {
+            char[] caracteres = texto.ToCharArray();
+            string invertida = "";
+
+            for (int i = caracteres.Length - 1; i >= 0; i--)
+            {
+                invertida += caracteres[i];
+            }
+
+            return invertida;
+        }
+    }
+}
